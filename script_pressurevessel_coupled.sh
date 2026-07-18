116759a75e58cba962c51da730e2919cf513c4f6
jungredda <ungredda@esteco.com>
2026-07-18 13:08:39 +0200

    coupled ucb real experiments

---
+++ script_pressurevessel_coupled.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+NRAND=40
+EXPERIMENT="pressurevessel"
+
+mkdir -p log out
+
+python run-multi-constraint-bo.py experiment_config/"$EXPERIMENT".json \
+  --num-rand $NRAND \
+  --acqfunc ucbc_coupled \
+  --plot 0 \
+  > log/log_ucbc_coupled_"$EXPERIMENT".txt \
+  2> log/err_ucbc_coupled_"$EXPERIMENT".txt
